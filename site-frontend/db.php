cd4490e2a75f6833e06ef57eeaeed8ac257ebb6a
Leonardo Nadson <72714982+leonardonadson@users.noreply.github.com>
2022-12-30 17:07:38 -0300

    Add files via upload

---
+++ site-frontend/db.php
@@ -0,0 +1,39 @@
+<?php
+
+class db
+{
+    private static $dbNome = 'canetaazul';
+    private static $dbHost = '192.168.100.20';
+    private static $dbUsuario = 'luisa';
+    private static $dbSenha = 'ifrn';
+    
+    private static $cont = null;
+    
+    public function __construct() 
+    {
+        die('A função Init nao é permitido!');
+    }
+    
+    public static function conectar()
+    {
+        if(null == self::$cont)
+        {
+            try
+            {
+                self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbNome, self::$dbUsuario, self::$dbSenha); 
+            }
+            catch(PDOException $exception)
+            {
+                die($exception->getMessage());
+            }
+        }
+        return self::$cont;
+    }
+    
+    public static function desconectar()
+    {
+        self::$cont = null;
+    }
+}
+
+?>
